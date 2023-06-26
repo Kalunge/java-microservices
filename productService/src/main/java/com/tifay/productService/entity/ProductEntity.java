d3849456273ecad876fb2eada663f6154fec824f
Titus Muthomi <muthomititus+github@gmail.com>
2023-06-26 11:18:57 +0300

    Add product repository

--- productService/src/main/java/com/tifay/productService/entity/ProductEntity.java
+++ productService/src/main/java/com/tifay/productService/entity/ProductEntity.java
@@ -1,12 +1,18 @@
 package com.tifay.productService.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-public class Product {
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
