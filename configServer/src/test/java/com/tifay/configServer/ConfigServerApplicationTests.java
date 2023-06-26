1c44e96b8a1f70da49bbf4f4ebd003bf9fa05065
Titus Muthomi <muthomititus+github@gmail.com>
2023-06-26 16:49:53 +0300

    Ad server config to remove rendundancy

---
+++ configServer/src/test/java/com/tifay/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tifay.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
