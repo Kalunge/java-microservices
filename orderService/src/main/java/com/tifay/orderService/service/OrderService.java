12e5a891caf8b7031d23846dcd826b5a5d14febc
Titus Muthomi <muthomititus+github@gmail.com>
2023-06-27 17:16:05 +0300

    Add logic to get order by id

--- orderService/src/main/java/com/tifay/orderService/service/OrderService.java
+++ orderService/src/main/java/com/tifay/orderService/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.tifay.orderService.service;
 
 import com.tifay.orderService.model.OrderRequest;
+import com.tifay.orderService.model.OrderResponse;
 import lombok.Data;
 
 public interface OrderService {
     long placeOrder(OrderRequest orderRequest);
+
+    OrderResponse getOrderDetails(long orderId);
 }
--- orderService/src/main/java/com/tifay/orderService/service/OrderServiceImplementation.java
+++ orderService/src/main/java/com/tifay/orderService/service/OrderServiceImplementation.java
@@ -1,10 +1,12 @@
 package com.tifay.orderService.service;
 
 import com.tifay.orderService.entity.Order;
+import com.tifay.orderService.exception.CustomException;
 import com.tifay.orderService.external.client.PaymentService;
 import com.tifay.orderService.external.client.ProductService;
 import com.tifay.orderService.external.request.PaymentRequest;
 import com.tifay.orderService.model.OrderRequest;
+import com.tifay.orderService.model.OrderResponse;
 import com.tifay.orderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,19 +48,28 @@ public class OrderServiceImplementation implements OrderService {
         PaymentRequest paymentRequest = PaymentRequest.builder().orderId(order.getId()).paymentMode(orderRequest.getPaymentMode()).orderAmount(orderRequest.getAmount()).build();
 
 
-         try {
-             paymentService.doPayment(paymentRequest);
-             log.info("Payment done successfully");
-             order.setOrderStatus("PLACED");
-         } catch (Exception e) {
-             log.error("Error occured in payment changing status to payment failed");
-             order.setOrderStatus("PAYMENT FAILED");
-         }
+        try {
+            paymentService.doPayment(paymentRequest);
+            log.info("Payment done successfully");
+            order.setOrderStatus("PLACED");
+        } catch (Exception e) {
+            log.error("Error occured in payment changing status to payment failed");
+            order.setOrderStatus("PAYMENT FAILED");
+        }
 
         order = orderRepository.save(order);
 
 
         log.info("Order placed successfully wth order id: {}", order.getId());
         return order.getId();
+    }
+
+    @Override
+    public OrderResponse getOrderDetails(long orderId) {
+        log.info("Get order details for order id " + orderId);
+
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new CustomException("Order Not found for the given id", "NOT_FOUND", 404));
+
+        return OrderResponse.builder().orderId(order.getId()).amount(order.getAmount()).orderDate(order.getOrderDate()).orderStatus(order.getOrderStatus()).build();
     }
 }
